dd09b44f2ef6b45b3acf4e5a511d4718ec4dbb5a
MehmetKaanErsoy <68550841+MehmetKaanErsoy@users.noreply.github.com>
2022-03-02 13:02:23 +0300

    first commit

---
+++ proje/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'proje'
---
+++ proje/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from pytube import YouTube
+import os
+from django.contrib import messages
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'anaekran.html')
+
+
+def download(request):
+    a = 100
+    global search
+    search = request.GET.get('search')
+    video = YouTube(search)
+    title = video.title
+    cozunurlukler_list = []
+    cozunurlukler = video.streams.filter(progressive=True).all()
+    for i in cozunurlukler:
+        cozunurlukler_list.append(i.resolution)
+
+    cozunurluk_list = list(dict.fromkeys(cozunurlukler_list))
+    return render(request, 'download.html',
+                  {'list': cozunurluk_list, 'search': search, 'cozunurlukler': cozunurlukler
+                      , 'title': title})
+
+
+def indirme_bitti(request, resolution):
+    global search
+    homedir = os.path.expanduser("~")
+    uzantı = homedir + '/Downloads'
+    YouTube(search).streams.get_by_resolution(resolution).download(uzantı)
+    return redirect('home')
